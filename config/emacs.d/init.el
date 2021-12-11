f8357eeff38c957dfe06b9a3dca6435d2695a427
Swalawaga <46899390+Swalawaga@users.noreply.github.com>
2021-12-11 16:15:22 -0500

    Delete config/emacs.d directory

--- config/emacs.d/init.el
+++
@@ -1,7 +0,0 @@
-;;load real config file 
-(setq custom-file (expand-file-name "config.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
-
-;;ShowNumbers
-;;(global-display-line-numbers-mode))
